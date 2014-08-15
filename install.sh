0c1850128973586c4db378f7e5a7d6138a3f2e5d
Tony Headford <tonyheadford@warwickshire.gov.uk>
2014-08-15 15:21:35 +0100

    Install.sh for clean installs with no rake. Removed Ultisnip vim plugin

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# link files
+for f in *
+do
+	SRC="${PWD}/${f}"
+	DST="${HOME}/.${f}"
+
+	if [ -f $DST ]; then
+		echo "backing up existing ${DST}"
+		mv $DST "${DST}.bak"
+	fi
+	echo "creating link to ${DST}"
+	ln -s $SRC $DST
+done
+
+# install vundle
+echo "installing vundle"
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+vim +BundleInstall +qall
+
+# install gitignore
+echo "adding global gitignore"
+git config --global core.excludesfile $HOME/.gitignore_global
+
+
